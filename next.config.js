880e7c4cd84b358eea1c22218098d2869a96a74f
ishibashi0112 <a_love.i_w_b_w_y.ya22@ezweb.ne.jp>
2021-09-26 23:12:57 +0900

    ✨ firebaseの導入

--- next.config.js
+++ next.config.js
@@ -1,3 +1,12 @@
 module.exports = {
   reactStrictMode: true,
+  env: {
+    FIREBASE_API_KEY: "AIzaSyAj3e65bODDi6MMQxjddS9lk-9CKRkQYdg",
+    FIREBASE_AUTH_DOMAIN: "memo-app-e06fb.firebaseapp.com",
+    FIREBASE_DATABASE_URL: "https://memo-app-e06fb.firebaseio.com",
+    FIREBASE_PROJECT_ID: "memo-app-e06fb",
+    FIREBASE_STORAGE_BUCKET: "memo-app-e06fb.appspot.com",
+    FIREBASE_MESSAGING_SENDER_ID: "1033143510239",
+    FIREBASE_APP_ID: "1:1033143510239:web:f18dae5a55b9d5602ef56a",
+  },
 };
