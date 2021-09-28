b31e987fb12638160123bfd76fd6490a8b923020
ishibashi0112 <a_love.i_w_b_w_y.ya22@ezweb.ne.jp>
2021-09-28 23:49:32 +0900

    ✨

--- src/components/Form.jsx
+++ src/components/Form.jsx
@@ -1,9 +1,21 @@
-import React from "react";
+import React, { useState } from "react";
 
 export default function Form() {
+  const [text, setText] = useState("");
+
+  const handleChange = (e) => {
+    setText(e.target.value);
+    console.log(text);
+  };
+
   return (
     <div className="w-2/3">
-      <input type="text" className="w-2/3 h-3/6 border-2" />
+      <input
+        type="text"
+        value={text}
+        onChange={handleChange}
+        className="w-2/3 h-3/6 border-2"
+      />
     </div>
   );
 }
--- src/components/Lists.jsx
+++ src/components/Lists.jsx
@@ -1,9 +1,24 @@
-import React from "react";
+import React, { useState } from "react";
 
 export default function Lists() {
+  const [memos] = useState([
+    { id: 1, body: "aaaaaaaa" },
+    { id: 2, body: "bbbbbbbb" },
+    { id: 3, body: "cccccccc" },
+  ]);
+
   return (
     <div className="w-1/3">
       <h1>メモ一覧</h1>
+      <ul>
+        {memos.map((memo) => {
+          return (
+            <li key={memo.id} className="w-full h-16">
+              {memo.body}
+            </li>
+          );
+        })}
+      </ul>
     </div>
   );
 }
